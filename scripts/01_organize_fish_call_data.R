e726bd04c2b5e6f5d76ad4f179295efd5d073654
skraftarcher <sarcher@lumcon.edu>
2022-04-29 14:17:44 -0500

    create an individual script for the autodetections organization function and clean up main script

--- scripts/01_organize_fish_call_data.R
+++ scripts/01_organize_fish_call_data.R
@@ -21,47 +21,15 @@ glimpse(lb)
 
 table(lb$Class)
 
-# organizing by minute
-
-fishcall.bymin.auto<-function(seltable){ # this is a function that can be used to 
-  # summarize all automatic detector datasets to get the number of fish calls by minute
-  
-  lb2<-seltable%>% 
-  separate(`Begin File`,
-           # starting from right and using negatives to allow stid lengths to differ
-           into=c("stid","yr","mnth","d","hr","min","sec","ext"),
-           sep = c(-16,-14,-12,-10,-8,-6,-4))%>%
-  select(-ext)%>%
-  mutate(FileDate=ymd_hms(paste(yr,mnth,d,hr,min,sec)),# this is the date-time the file started recording
-         CallDate=FileDate+`File Offset (s)`,# this is the date-time the fish call actually occurred
-         # the following replaces the year,month day, min, sec variables with the fish call specific values
-         yr=year(CallDate),
-         mnth=month(CallDate),
-         d=day(CallDate),
-         hr=hour(CallDate),
-         min=minute(CallDate),
-         sec=second(CallDate))
-
-# now summarize by minute to compare to manual detections
-
-lb.min<-lb2%>%
-  #create a variable that is 1 if its a fish call and 0 if it was classified as noise
-  mutate(fishcall=ifelse(Class=="FS",1,0))%>%
-  # group by unique minutes
-  group_by(yr,mnth,d,hr,min)%>%
-  # get total number of fish calls
-  summarize(nfishcall=sum(fishcall))
-  
-# this is the data you want the funtion to return
-return(lb.min)
-} # this closes the function
+# bring in functions we've written
+source("scripts/organizemanualchecks.R")
+source("scripts/organizeautodetections.R")
 
 # this is how you call the function
 
 function.check<-fishcall.bymin.auto(seltable=lb)
-
-
-
-
-
+trial<-man.check.organize("LionsBayDectionsCheck_reviewer3.txt")
+names(trial)
+trial[[4]]
+trial$by.min.summary
 
---
+++ scripts/organizeautodetections.R
@@ -0,0 +1,37 @@
+# Script to organize automatic detections
+# Glass sponge reef acoustics project 
+
+#Glass sponge reef working group 4/29/2022
+
+fishcall.bymin.auto<-function(seltable){ # this is a function that can be used to 
+  # summarize all automatic detector datasets to get the number of fish calls by minute
+  
+  lb2<-seltable%>% 
+    separate(`Begin File`,
+             # starting from right and using negatives to allow stid lengths to differ
+             into=c("stid","yr","mnth","d","hr","min","sec","ext"),
+             sep = c(-16,-14,-12,-10,-8,-6,-4))%>%
+    select(-ext)%>%
+    mutate(FileDate=ymd_hms(paste(yr,mnth,d,hr,min,sec)),# this is the date-time the file started recording
+           CallDate=FileDate+`File Offset (s)`,# this is the date-time the fish call actually occurred
+           # the following replaces the year,month day, min, sec variables with the fish call specific values
+           yr=year(CallDate),
+           mnth=month(CallDate),
+           d=day(CallDate),
+           hr=hour(CallDate),
+           min=minute(CallDate),
+           sec=second(CallDate))
+  
+  # now summarize by minute to compare to manual detections
+  
+  lb.min<-lb2%>%
+    #create a variable that is 1 if its a fish call and 0 if it was classified as noise
+    mutate(fishcall=ifelse(Class=="FS",1,0))%>%
+    # group by unique minutes
+    group_by(yr,mnth,d,hr,min)%>%
+    # get total number of fish calls
+    summarize(nfishcall=sum(fishcall))
+  
+  # this is the data you want the funtion to return
+  return(lb.min)
+} # this closes the function
