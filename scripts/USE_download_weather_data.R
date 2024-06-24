be1fabfbccb612b42a5e70c387b52edf562ddd25
Stephanie Archer <sarcher@LUMCON.edu>
2024-06-24 10:16:33 -0500

    script to collect weather data

---
+++ scripts/USE_download_weather_data.R
@@ -0,0 +1,43 @@
+#script to pull in weather data
+#load packages
+if(!require(weathercan))devtools::install_github("ropensci/weathercan")
+if(!require(tidyverse))install.packages("tidyverse");library(tidyverse)
+if(!require(readxl))install.packages("readxl");library(readxl)
+
+
+# find weather stations
+stations_dl()
+stations_search("Bonilla")
+
+#download weather data
+#bella bella
+bbwthr <- weathercan::weather_dl(station_ids = 49388,# use Bella Bella station
+                                 time_disp="UTC",
+                                 start = "2017-08-21", 
+                                 end = "2017-10-22")
+
+#lions bay
+lbwthr <- weathercan::weather_dl(station_ids = 6817,# use howe sound - Pam rocks station
+                                 time_disp="UTC",
+                                 start = "2017-09-30", 
+                                 end = "2017-11-30")
+
+#Hecate
+hwthr <- weathercan::weather_dl(station_ids = 8265,# Bonilla Island station
+                                 time_disp="UTC",
+                                 start = "2017-05-12", 
+                                 end = "2017-05-21")
+
+#organize our weather data
+#bella bella
+bbwthr2<-bbwthr%>%
+  select(date.time=time,precip_amt,temp,wind_dir,wind_spd)%>%
+  mutate()
+
+#lions bay
+lbwthr2<-lbwthr%>%
+  select(date.time=time,precip_amt,temp,wind_dir,wind_spd)
+
+#hecate
+hwthr2<-hwthr%>%
+  select(date.time=time,precip_amt,temp,wind_dir,wind_spd)
